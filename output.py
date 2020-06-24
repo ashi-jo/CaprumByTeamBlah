d8fae23bee9955e57cd80adfafc5e4db084744ce
003ash <64546134+003ash@users.noreply.github.com>
2020-06-24 17:20:38 +0530

    Update output.py

--- output.py
+++ output.py
@@ -4,42 +4,53 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity as cs
 import csv
 
+def get_title_from_index(index,mf):
+  return mf[mf.index == index]['title'].values[0]
 
-def get_title_from_index(index):
-  return movies_file[movies_file.index == index]['title'].values[0]
+
+def get_index_from_title(title,mf):
+  return mf[mf.title == title]['movieId'].values[0]
 
 
-def get_index_from_title(title):
-  return movies_file[movies_file.title == title]['movieId'].values[0]
+def get_genre_from_title(title,mf):
+  return mf[mf.title == title]['genres'].values[0]
 
-movies_file = pd.read_csv('movies-wpu.csv', engine='python')
-movie_user_likes = "Toy Story (1995)"#input('>  ')
-req_row_list_1 = []
-#filehandle = pd.read_csv('scores.csv', names=list(range(8719)),engine='python')
-with open('scores.csv') as scores:
-  movie_index = get_index_from_title(movie_user_likes)
-  rows = csv.reader(scores)
-  for row in enumerate(rows):
-    if row[0] == movie_index:
-      for i in row[1]:
-        i = (float(i.split(',')[0].split('(')[1]),float(i.split(',')[1].split(')')[0]))
-        req_row_list_1.append(i)
-      #sort the required list in major list to get descending order of similar movies
-      sorted_similar_movie = sorted(req_row_list_1, key=lambda x:x[1], reverse = True)
 
-  #req_row = filehandle.iloc[movie_index - 1]
+def get_imageurl_from_title(title,mf):
+  return mf[mf.title == title]['imageUrl'].values[0]
 
-  #req_row_list = list(req_row.replace(nm.nan, ' ', regex=True))
 
-  #req_row_list_1 = []
-  #while ' ' in req_row_list:
-  #  req_row_list.remove(req_row_list[-1])
-  #print(req_row_list)
+def get_plot_from_title(title,mf):
+  return mf[mf.title == title]['plot'].values[0]
+
+
+
+def output(name):
   
-
-
-for movies in sorted_similar_movie:
-  print(get_title_from_index(movies[0]))
+  movies_file = pd.read_csv('movies-wpu.csv', engine='python')
+  index = get_index_from_title(name,movies_file)
+  #movie_user_likes = "Jumanji (1995)"#input('>  ')
+  req_row_list_1 = []
+  movie_index = int(index) - 1
+  with open('scores.csv') as scores:
+    #movie_index = get_index_from_title(movie_user_likes)
+    rows = csv.reader(scores)
+    for row in enumerate(rows):
+      if row[0] == movie_index:
+        for i in row[1]:
+          i = (float(i.split(',')[0].split('(')[1]),float(i.split(',')[1].split(')')[0]))
+          req_row_list_1.append(i)
+        #sort the required list in major list to get descending order of similar movies
+        sorted_similar_movie = sorted(req_row_list_1, key=lambda x:x[1], reverse = True)
+  final_list=[]  
+  counter = 0
+  for movies in sorted_similar_movie:
+    counter += 1
+    movie_name = get_title_from_index(movies[0],movies_file)
+    if counter > 15:
+      return final_list
+    f_list = {"name":movie_name, "genre":get_genre_from_title(movie_name,movies_file),"imageurl":get_imageurl_from_title(movie_name,movies_file),"plot":get_plot_from_title(movie_name,movies_file)}
+    final_list.append(f_list) 
 
 
 
